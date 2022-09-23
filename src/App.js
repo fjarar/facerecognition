9d21f52ca7bab72a41ec57078139bfe8d03d911a
fjarar <flavio.jara.rebolledo@gmail.com>
2022-09-23 14:42:44 -0300

    added some packages and new imagelink component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,42 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 import 'tachyons';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* 
-      <FaceRecognition /> */}
-    </div>
-  );
+
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (e) => {
+    console.log(e.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+  }
+
+  render(){
+    return (
+      <div className="App">
+      <ParticlesBg type='cobweb' bg={true}/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {/* 
+        <FaceRecognition /> */}
+      </div>
+    );
+  }  
 }
 
 export default App;
