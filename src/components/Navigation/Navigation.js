ee4faa83e885bd0a7246ed07a4421ef4677e90bd
fjarar <flavio.jara.rebolledo@gmail.com>
2022-09-23 10:43:32 -0300

    facerecognitionapp first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navigation(){
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className="f3 link dim blak underline pa3 pointer">Sign Out</p>
+        </nav>
+    );
+
+}
+
+export default Navigation;
